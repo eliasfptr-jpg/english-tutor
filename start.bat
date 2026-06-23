c59bf43ecb97dbb74eab9ca3cc740ba2ef2a7cc5
eliasfptr-jpg <ilyashamlaoui48@gmail.com>
2026-06-23 09:41:39 +0100

    Add start.bat

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting English Tutor AI...
+echo Make sure OPENCODE_API_KEY environment variable is set.
+echo Get a free key at https://opencode.ai/zen
+echo.
+node server.js
+pause
